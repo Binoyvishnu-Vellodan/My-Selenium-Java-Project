d1013776bca954c5975c96e964bed95943f9d104
Binoy Vishnu <binoyvishnuv@gmail.com>
2025-06-22 22:07:43 -0400

    new change

--- src/test/java/org/Selenium/New.java
+++ src/test/java/org/Selenium/New.java
@@ -2,10 +2,14 @@ package org.Selenium;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class New {
-    public static void main(String args[]){
+    @Test(groups = "regression")
+    public void Data(){
         WebDriver driver=new ChromeDriver();
         driver.get("https://www.google.com/");
+        driver.quit();
+        driver.close();
     }
 }
